291af524f6c75758d99e6312bfabb493e0a5aff3
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-09-14 10:52:28 -0700

    Implemented alternating players

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,22 @@
 console.log('Connected');
 
+var player = "X";
 // toggles block by id when clicked
 function toggle(block) {
-  console.log(block);
-  document.getElementById(block).innerHTML = "X";
+  var space = document.getElementById(block).innerHTML;
+  // only toggle if its a blank space
+  if (space === "") {
+    console.log(block);
+    document.getElementById(block).innerHTML = player;
+    // change player
+    player = nextPlayer(player);
+  }
+}
+
+// alternate players
+function nextPlayer(str) {
+  if (str === "X") {
+    return "O";
+  }
+  return "X";
 }
