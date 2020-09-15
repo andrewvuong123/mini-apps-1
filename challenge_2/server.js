1257edc49ea8d7e3f089f86b41cb45354dc6c36a
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-09-15 12:22:33 -0700

    passing form data to the server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
+// serves up index file in client
+app.use(express.static('client'));
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.post('/upload_json', function (req, res) {
+  console.log(req.data);
+  console.log(JSON.parse(req.body['json-text']));
+  res.status(200).send();
+  // process JSON data to csv
+  // insert result into response html
+  // render new page with form and csv report files
+})
+
+
+// listens on localhost
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
 
