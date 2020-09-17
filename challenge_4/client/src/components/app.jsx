bc92faa4fa8eefefddb2a205e23dcb81b0bd3c2c
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-09-17 10:37:24 -0700

    setup files

---
+++ challenge_4/client/src/components/app.jsx
@@ -0,0 +1,12 @@
+// main component
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello, world!</h1>
+    )
+  }
+}
+
+export default App;
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+app.listen(port, () => {
+  console.log(`Connect 4 app listening at http://localhost:${port}`)
+});
 
